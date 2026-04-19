ac6deb65f4dbb326a1e48911869996c1d688786a
Sabari Sundaresan <sundaresansabari@gmail.com>
2026-04-19 08:53:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -409,4 +409,4 @@ def download_appointment(id):
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port = int(os.getenv("PORT", 5000)))
